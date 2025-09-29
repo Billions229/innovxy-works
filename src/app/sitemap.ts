bbe154dce644292a3c6ce78ce4c0ffe87f637e7b
hris25 <164737303+hris25@users.noreply.github.com>
2025-09-29 14:04:43 +0100

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,32 +1,32 @@
-import { MetadataRoute } from 'next'
+import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://innovyxworks.com'
-  
+  const baseUrl = "https://innovyxworks.com";
+
   return [
     {
       url: baseUrl,
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
+      lastModified: new Date().toISOString(),
+      changeFrequency: "weekly",
       priority: 1,
     },
     {
       url: `${baseUrl}/privacy`,
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
+      lastModified: new Date().toISOString(),
+      changeFrequency: "monthly",
       priority: 0.5,
     },
     {
       url: `${baseUrl}/terms`,
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
+      lastModified: new Date().toISOString(),
+      changeFrequency: "monthly",
       priority: 0.5,
     },
     {
       url: `${baseUrl}/cookies`,
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
+      lastModified: new Date().toISOString(),
+      changeFrequency: "monthly",
       priority: 0.5,
     },
-  ]
+  ];
 }
